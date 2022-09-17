3331aa041f272fdefd1d630f015025199ccdff07
Sealkeen <lettersforlotro@gmail.com>
2022-09-17 22:39:21 +0300

    Modified css and JS to newer version (update)

--- js/api.js
+++ js/api.js
@@ -1,3 +1,11 @@
 export default {
-    loc: 'https://8a2b-37-144-215-79.eu.ngrok.io/'
+    loc: 'https://8a2b-37-144-215-79.eu.ngrok.io/',
+    runAtGithub: function() 
+    {
+        if (window.location.href.indexOf("franky") > -1) 
+        {
+            return true
+        }
+        return false
+    }
 }
