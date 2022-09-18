e0b1f3507688031b0358e2d962618427a114ab97
Sealkeen <lettersforlotro@gmail.com>
2022-09-18 23:56:28 +0300

    hotfix #3

---
+++ js/StyleHandlers/color-handlers.js
@@ -0,0 +1,13 @@
+
+export default{ 
+    toggleBodyBackground()
+    {
+        if ($('body').hasClass('white')) {
+            $('body').toggleClass('white');
+            //$('body').css('background-color', 'white');
+        } else {
+            $('body').toggleClass('white');
+            //$('body').css('background-color', 'grey');
+        }
+    }
+}
