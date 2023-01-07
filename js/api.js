5212d6250fdae8add156b67d3d982db930e0d1eb
Sealkeen <lettersforlotro@gmail.com>
2023-01-07 12:28:15 +0300

    Removed unused import.

--- js/api.js
+++ js/api.js
@@ -1,7 +1,7 @@
 const urls = {
     getLocation() {
         if(this.isGithub()) {
-            return 'https://798b-89-178-156-223.eu.ngrok.io/'
+            return 'https://da99-37-144-212-217.eu.ngrok.io/'
         } else if (this.isNodeJSHost()) {
             return 'http://localhost:8080/'
         } else
