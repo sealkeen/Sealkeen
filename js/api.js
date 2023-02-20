66ebce05e3760f4a9dcbfc01350e658aab866bf5
Sealkeen <lettersforlotro@gmail.com>
2023-02-21 00:00:41 +0300

    Removed unused code in click-handlers.js

--- js/api.js
+++ js/api.js
@@ -1,7 +1,7 @@
 const urls = {
     getLocation() {
         if(this.isGithub()) {
-            return 'https://7ec7-37-144-213-180.eu.ngrok.io/'
+            return 'https://b4ff-89-178-157-251.eu.ngrok.io'
         } else if (this.isNodeJSHost()) {
             return 'https://localhost:5001/'
         } else
