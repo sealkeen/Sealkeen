24edde0170c9941746ca5ba9359b2e281a41001c
Sealkeen <lettersforlotro@gmail.com>
2023-05-22 16:14:23 +0300

    Updated api host...

--- js/api.js
+++ js/api.js
@@ -1,7 +1,7 @@
 const urls = {
     getLocation() {
         if(this.isGithub() || this.isNgrok()) {
-            return 'https://b791-37-144-214-109.ngrok-free.app/';
+            return 'https://0988-89-178-156-53.ngrok-free.app/';
         } else if ( this.isRemoteWorkspace() ) {
             return 'https://localhost:443/'
         } else if (!this.isNodeJSHost()) {
