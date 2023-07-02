499afa0600bcda6e51ca391bbc01d8acf3f32113
Sealkeen <lettersforlotro@gmail.com>
2023-07-02 15:53:35 +0300

    add try error handling

--- js/Services/Localization/fill-localization-store.js
+++ js/Services/Localization/fill-localization-store.js
@@ -53,50 +53,55 @@ function translateGreetings(greetingKey)
 
 function AddLanguageDropDown()
 {  
-    const dropdownToggles = document.querySelectorAll('.dropdown-toggle');
-    if (dropdownToggles.length > 0) {
-        return; // Exit the function
-    }
-    // Continue with the function
-    console.log("No dropdown toggles found");
+    try {
+        const dropdownToggles = document.querySelectorAll('.dropdown-toggle');
+        if (dropdownToggles.length > 0) {
+            return; // Exit the function
+        }
+        // Continue with the function
+        console.log("No dropdown toggles found");
 
-    // Create language combobox button
-    const languageComboBoxButton = document.createElement('li');
-    languageComboBoxButton.classList.add('nav-item', 'dropdown', 'language-combobox-button');
+        // Create language combobox button
+        const languageComboBoxButton = document.createElement('li');
+        languageComboBoxButton.classList.add('nav-item', 'dropdown', 'language-combobox-button');
 
-    const languageComboBoxButtonLink = document.createElement('a');
-    languageComboBoxButtonLink.classList.add('nav-link', 'dropdown-toggle', 'text-dark', 'stroke-shadow-h3-white');
-    languageComboBoxButtonLink.href = '#';
-    languageComboBoxButtonLink.setAttribute('data-toggle', 'dropdown');
-    languageComboBoxButtonLink.setAttribute('aria-haspopup', 'true');
-    languageComboBoxButtonLink.setAttribute('aria-expanded', 'false');
-    languageComboBoxButtonLink.textContent = lS.getDefaultLanguage();
+        const languageComboBoxButtonLink = document.createElement('a');
+        languageComboBoxButtonLink.classList.add('nav-link', 'dropdown-toggle', 'text-dark', 'stroke-shadow-h3-white');
+        languageComboBoxButtonLink.href = '#';
+        languageComboBoxButtonLink.setAttribute('data-toggle', 'dropdown');
+        languageComboBoxButtonLink.setAttribute('aria-haspopup', 'true');
+        languageComboBoxButtonLink.setAttribute('aria-expanded', 'false');
+        languageComboBoxButtonLink.textContent = lS.getDefaultLanguage();
 
-    languageComboBoxButton.appendChild(languageComboBoxButtonLink);
+        languageComboBoxButton.appendChild(languageComboBoxButtonLink);
 
-    // Create language options dropdown menu
-    const languageOptionsDropdownMenu = document.createElement('div');
-    languageOptionsDropdownMenu.classList.add('dropdown-menu');
+        // Create language options dropdown menu
+        const languageOptionsDropdownMenu = document.createElement('div');
+        languageOptionsDropdownMenu.classList.add('dropdown-menu');
 
-    lS.getLanguages().forEach((language) => {
-        const languageOptionLink = document.createElement('a');
-        languageOptionLink.classList.add('dropdown-item');
-        languageOptionLink.textContent = language;
+        lS.getLanguages().forEach((language) => {
+            const languageOptionLink = document.createElement('a');
+            languageOptionLink.classList.add('dropdown-item');
+            languageOptionLink.textContent = language;
 
-        languageOptionLink.addEventListener('click', () => {
-            console.log('Setting default language: ' + language)
-            lS.setDefaultLanguage(language);
-            window.translateWebsite();
-            // Get the language toggler and set its text to the default language
-            const languageToggler = document.querySelector('.language-combobox-button .nav-link');
-            languageToggler.textContent = language;
+            languageOptionLink.addEventListener('click', () => {
+                console.log('Setting default language: ' + language)
+                lS.setDefaultLanguage(language);
+                window.translateWebsite();
+                // Get the language toggler and set its text to the default language
+                const languageToggler = document.querySelector('.language-combobox-button .nav-link');
+                languageToggler.textContent = language;
+            });
+
+            languageOptionsDropdownMenu.appendChild(languageOptionLink);
         });
 
-        languageOptionsDropdownMenu.appendChild(languageOptionLink);
-    });
+        languageComboBoxButton.appendChild(languageOptionsDropdownMenu);
 
-    languageComboBoxButton.appendChild(languageOptionsDropdownMenu);
+        const navbarUserRegisterLogin = document.querySelector('#navbar-nav-user-register-login');
+        navbarUserRegisterLogin.insertBefore(languageComboBoxButton, navbarUserRegisterLogin.firstChild);
+    } 
+    catch {
 
-    const navbarUserRegisterLogin = document.querySelector('#navbar-nav-user-register-login');
-    navbarUserRegisterLogin.insertBefore(languageComboBoxButton, navbarUserRegisterLogin.firstChild);
+    } 
 }
