273a8ab89b6566a21ed6ea1543e9774a4fca201c
Sealkeen <lettersforlotro@gmail.com>
2023-07-02 16:00:31 +0300

    Add try-catch on window resize [2]

--- js/StyleHandlers/navbar-handlers.js
+++ js/StyleHandlers/navbar-handlers.js
@@ -1,30 +1,32 @@
 export function addEventOnWindowResize() {
-    document.querySelector('.spacer').style.height = $('.navbar').height() * 1.5 + 'px';
-    console.log('Adding sidenav resize handlers...')
-    $('.sidenav').each(function() {
-        let navHeight = $('nav').outerHeight();
-        $(this).css('top', navHeight);
-    });
-
-    $('.navbar-toggler').on('click', function() {
-        $('nav').one('transitionend', function() {
-          let navHeight = $(this).outerHeight();
-          $('.sidenav').each(function() {
-            //console.log('<nav>\'s transition end has fired into sidenav.')
-            $(this).css('top', navHeight);
-          });
-        });
-      });
-
-    window.addEventListener('resize', function(event) {
-        //console.log('size changed:' + $('.navbar').height() + 'px');  
-
+    try {
         document.querySelector('.spacer').style.height = $('.navbar').height() * 1.5 + 'px';
-
-        // sidenavs on resize
-        let navHeight = $('nav').outerHeight();
+        console.log('Adding sidenav resize handlers...')
         $('.sidenav').each(function() {
+            let navHeight = $('nav').outerHeight();
             $(this).css('top', navHeight);
         });
-    }, true);
+
+        $('.navbar-toggler').on('click', function() {
+            $('nav').one('transitionend', function() {
+            let navHeight = $(this).outerHeight();
+            $('.sidenav').each(function() {
+                //console.log('<nav>\'s transition end has fired into sidenav.')
+                $(this).css('top', navHeight);
+            });
+            });
+        });
+
+        window.addEventListener('resize', function(event) {
+            //console.log('size changed:' + $('.navbar').height() + 'px');  
+
+            document.querySelector('.spacer').style.height = $('.navbar').height() * 1.5 + 'px';
+
+            // sidenavs on resize
+            let navHeight = $('nav').outerHeight();
+            $('.sidenav').each(function() {
+                $(this).css('top', navHeight);
+            });
+        }, true);
+    } catch {}
 }
