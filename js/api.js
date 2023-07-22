88113f4d73ca6c7f2859829458de2d7b8d5c9546
Sealkeen <lettersforlotro@gmail.com>
2023-07-22 20:13:13 +0300

    [tnl] upd

--- js/api.js
+++ js/api.js
@@ -1,7 +1,7 @@
 const urls = {
     getLocation() {
         if(this.isGithub()) {
-            return 'https://53d2-185-18-125-92.ngrok-free.app/';
+            return 'https://b5cb-185-18-125-92.ngrok-free.app/';
         } else if ( this.isRemoteWorkspace() ) {
             return 'https://localhost:443/';
         } else if ( !this.isNodeJSHost() || this.isNgrok() ) {
