b745e3cfc81054e91e686a59b89c9b81df71fd55
Sealkeen <lettersforlotro@gmail.com>
2023-08-05 21:41:30 +0300

    add missing imports

--- js/Account/Identity/sign-up.js
+++ js/Account/Identity/sign-up.js
@@ -1,4 +1,5 @@
 import { onSiteLoadIfAuthorized } from "../../Router/testing.js";
+import { createInfoMessage } from "../../../Sealkeen/js/Errors/fetch-errors.js";
 
 createInfoMessage("Step in - REGISTER page.");
 onSiteLoadIfAuthorized(true);
