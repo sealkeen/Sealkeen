eb7a78d70825e8fa95a7891884ff918a622b49a4
Sealkeen <lettersforlotro@gmail.com>
2024-02-23 19:00:29 +0300

    [tnl] update host url

--- js/api.js
+++ js/api.js
@@ -8,7 +8,7 @@ const urls = {
         if (window.hostLocation != null) {
             return window.hostLocation; // custom location set from command line if needed
         } else if (this.isGithub()) {
-            return 'https://7078-31-134-149-42.ngrok-free.app/';
+            return 'https://12fd-31-134-149-42.ngrok-free.app/';
         } else if ( this.isRemoteWorkspace() ) {
             return 'https://localhost:443/';
         } else if ( !this.isNodeJSHost() || this.isNgrok() ) {
