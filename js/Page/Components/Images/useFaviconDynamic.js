3b99f964a38d532400c4e1abb0f3b509ab01a10e
Sealkeen <lettersforlotro@gmail.com>
2024-11-02 17:06:50 +0300

    [FSD.RF] Use fav icon dynamic component, correct margin.

---
+++ js/Page/Components/Images/useFaviconDynamic.js
@@ -0,0 +1,9 @@
+export function useFaviconDynamic() {
+    var link = document.querySelector("link[rel~='icon']");
+    if (!link) {
+        link = document.createElement('link');
+        link.rel = 'icon';
+        document.head.appendChild(link);
+    }
+    link.href = 'favicon.ico';
+}
--- js/Page/index.js
+++ js/Page/index.js
@@ -1,8 +1,10 @@
 import { useTapCircle } from "./Components/ScreenStyles/tap-circle.js";
 import { useVirtualMouseIfMobile } from './Components/Inputs/virtual-mouse.js'
+import { useFaviconDynamic } from "./Components/Images/useFaviconDynamic.js";
 
 useVirtualMouseIfMobile();
 export function usePageModifyingComponents() {
     useVirtualMouseIfMobile();
     useTapCircle();
+    useFaviconDynamic();
 }
